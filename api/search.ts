3d36fae640cfd16ada34e5e5291c77b627a4df4d
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-09 17:31:00 +0700

    add search

--- api/search.ts
+++ api/search.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import { conn } from "../bdconnect";
 import { Movie, creators, stars } from "../mobel/mobel_search";
 import mysql from "mysql";
@@ -9,33 +9,33 @@ export const router = express.Router();
 export const queryAsync = util.promisify(conn.query).bind(conn);
 
 router.get("/:input", async (req,res)=>{
-    // let input = req.params.input;
-    let input = '%' + req.params.input + '%';
-    let data : Movie | undefined;
+    
+let input = '%' + req.params.input + '%';
 
-    const sql = mysql.format('SELECT * FROM Movie WHERE Mo_name LIKE ?', [input]);
-    let result = await queryAsync(sql);
-    const rawData = JSON.parse(JSON.stringify(result));
-    console.log(rawData);
-    data = rawData[0] as Movie;
-    console.log(data.Mid);
+const sql = mysql.format('SELECT * FROM Movie WHERE Mo_name LIKE ?', [input]);
+let result = await queryAsync(sql);
 
-    const sql_stars = mysql.format('SELECT * FROM MStars WHERE Mid = ?', [data.Mid]);
-    let result2 = await queryAsync(sql_stars);
-    const starsData = JSON.parse(JSON.stringify(result2)) as stars[];
-    console.log(starsData);
+const rawData = JSON.parse(JSON.stringify(result));
+console.log(rawData);
 
-    data.stars = starsData;
-    console.log(data); 
+let data: (Movie & { stars: stars[], creators: creators[] })[] = [];
 
-    const sql_creators = mysql.format('SELECT * FROM MCreator WHERE Mid = ?', [data.Mid]);
-    let result3 = await queryAsync(sql_creators);
-    const creatorsData = JSON.parse(JSON.stringify(result3)) as creators[];
-    console.log(creatorsData);
-    data.creators = creatorsData;
+for (let i = 0; i < rawData.length; i++) {
+    let movieData = rawData[i] as Movie;
+    console.log(movieData.Mid);
+
+    const sql_stars = mysql.format('SELECT * FROM MStars WHERE Mid = ?', [movieData.Mid]);
+    let starsResult = await queryAsync(sql_stars);
+    const starsData = JSON.parse(JSON.stringify(starsResult)) as stars[];
+    
+    const sql_creators = mysql.format('SELECT * FROM MCreator WHERE Mid = ?', [movieData.Mid]);
+    let creatorsResult = await queryAsync(sql_creators);
+    const creatorsData = JSON.parse(JSON.stringify(creatorsResult)) as creators[];
+
+    data.push({ ...movieData, stars: starsData, creators: creatorsData });
+}
     console.log(data);
-
     res.json(data);
-  
- 
+    
 });
+
